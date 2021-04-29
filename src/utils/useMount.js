faee771cc2f5cd0eedf27e3d0ce50bd66891e754
askwuxue <972523419@qq.com>
2021-04-29 22:43:27 +0800

    feat: add useMount HOOK at first render and once

---
+++ src/utils/useMount.js
@@ -0,0 +1,10 @@
+// 实现一个自定义HOOK对同一的函数进行封装
+import { useEffect } from "react";
+
+const useMount = function (callback) {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export default useMount;

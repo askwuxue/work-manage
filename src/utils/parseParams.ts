75f95aa2d0b2dbb202fca5805dfd0c812c4d486a
askwuxue <972523419@qq.com>
2021-04-30 00:08:09 +0800

    refactor: .js all in .jsx try use typescript

--- src/utils/parseParams.ts
+++ src/utils/parseParams.ts
@@ -1,9 +1,10 @@
 import qs from "querystring";
 
 // 处理请求参数，对参数中value为空且不为零的参数在参数对象中删除
-function parseParams(object) {
+function parseParams(object: object): string {
   // TODO 对象的结构赋值
-  let newObj = { ...object };
+  let newObj: any = { ...object };
+
   // TODO syntactic Object.keys(obj)
   let keys = Object.keys(newObj);
   keys.forEach((key) => {

c9c3632421ce1cd1bc17a99aff7d4009685d1eff
askwuxue <972523419@qq.com>
2021-05-19 23:11:27 +0800

    feat: add login and register logic

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const Router = require("koa-router");
 const app = new Koa();
 const router = new Router();
 const bodyParser = require("koa-bodyparser");
+// const jwtAuth = require("koa-jwt");
+const jwt = require("jsonwebtoken");
+// const { koaJwtSecret } = require('jwks-rsa');
+// const privateKey = 'hello';
 
 app.use(bodyParser());
 
@@ -12,23 +16,58 @@ app.use(async (ctx, next) => {
     ctx.set({
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Custom-Header, Content-Type",
+      "Access-Control-Allow-Headers": "Content-Type, bearer",
     });
   } else {
-    ctx.set("Access-Control-Allow-Origin", "*");
+    ctx.set({
+      "Access-Control-Allow-Origin": "*",
+    });
   }
   next();
 });
+
+// 首页
 router.get("/", async (ctx) => {
   ctx.body = "/";
 });
 
+// 注册
 router.post("/register", async (ctx) => {
   ctx.res.statusCode = 200;
   ctx.set("Access-Control-Allow-Origin", "*");
-  const { body } = ctx.request;
-  console.log(body);
-  ctx.body = "body";
+  // TODO 使用console打印看不到body直接访问
+  const { body } = await ctx.request;
+  const token = jwt.sign(
+    {
+      // 设置过期时间
+      exp: Math.floor(Date.now() / 1000) + 5,
+      data: "register",
+    },
+    "secret"
+  );
+  ctx.body = {
+    token,
+    username: body.username,
+  };
+});
+
+// 登录
+router.post("/login", async (ctx) => {
+  ctx.res.statusCode = 200;
+  ctx.set("Access-Control-Allow-Origin", "*");
+  // TODO 使用console打印看不到body直接访问
+  const { bearer } = await ctx.request.headers;
+  // 对token进行解码
+  // const decode = jwt.verify(bearer, "secret");
+  // 对token进行验证，如果token不一致。验证失败
+  jwt.verify(bearer, "secret", function (err, data) {
+    if (err) {
+      console.log("login failed");
+    } else {
+      console.log("login success");
+    }
+  });
+  ctx.body = {};
 });
 
 app.use(router.routes());
